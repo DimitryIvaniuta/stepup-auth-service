c002f7d645037f5c5df9ac72a1e42e1fe171523f
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2026-01-25 20:31:10 +0100

    StepUp Auth application

---
+++ src/main/java/com/github/dimitryivaniuta/gateway/stepupauth/StepUpAuthApplication.java
@@ -0,0 +1,17 @@
+package com.github.dimitryivaniuta.gateway.stepupauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Main entry point for the Step-Up Authentication (risk-based) service.
+ */
+@EnableScheduling
+@SpringBootApplication
+public class StepUpAuthApplication {
+    /** Boots the application. */
+    public static void main(String[] args) {
+        SpringApplication.run(StepUpAuthApplication.class, args);
+    }
+}
